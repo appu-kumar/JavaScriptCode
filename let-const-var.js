a133161865a7d988c6029e7e0b33c474f3bcb344
appu-kumar <83134946+appu-kumar@users.noreply.github.com>
2024-02-03 11:33:30 +0530

    Learned about let-const-var keyword

---
+++ let-const-var.js
@@ -0,0 +1,26 @@
+
+const accountId=93542569;                         // semicolon is an optional character.
+// const emailId;           invalid statement in js           // declaraction and initialization happen together ok in js. like accountId
+let name="appu kumar";
+var email="a.k@gmail.com";
+state="delhi";
+
+
+
+/////////////////  see can be change above variables or not
+
+// accountId=23244543;             // const variable can not be reinitialized okay
+name="a.k";
+var email="ram@gmail.com";        // reinitialization to thik hai, redeclaration kaise ho gya Note:: Do not use var keyword okay, only const and let use.
+state="punjab";
+
+
+
+console.log("for single variable");
+console.table([accountId,name,email,state]);
+
+
+
+
+
+
