71600bd089e037fb22d4b511a1f61985c50b190c
appu-kumar <83134946+appu-kumar@users.noreply.github.com>
2024-02-04 12:20:02 +0530

    datatypes convered in this file

---
+++ datatypes.js
@@ -0,0 +1,29 @@
+"use strict";                // this statement allows to write newer version of js.
+
+
+let age=35;           // number
+let names="rahul kumar";  // string
+let loggedIn=true   // boolean type
+let salary=8.8     // number type
+
+let mobileNumber=3543463363;    let week="sat";               // will not work just terminate the first statement--->now working--->but where is readability just write in the next line
+
+//alert(3+3);             // we r using node.js not browser okay
+
+let state;         // js engine will assign undefined value 
+let subject=null;      // null value means missing value not found okay
+
+/*
+     int--->2^56 around
+     string-->""
+     boolean -->true/false
+     undefined--->undefined
+     object---->{key:val,key:val}
+     null is object type ok    interview question
+
+
+*/
+
+console.log(typeof state);   // undefined
+console.log(typeof null);      // object
+console.log(typeof(salary));  // number
