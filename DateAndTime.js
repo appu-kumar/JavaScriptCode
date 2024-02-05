997e9b08a23a717dedc36dfe92a65413c332ebbf
appu-kumar <83134946+appu-kumar@users.noreply.github.com>
2024-02-05 12:13:13 +0530

    Date and Time

---
+++ DateAndTime.js
@@ -0,0 +1,14 @@
+console.log("hello");
+
+
+const date=new Date();                     // 1 jan 1970 se time batata hai
+console.log(date.toString());
+
+console.log(date.getTime());
+console.log(date.toDateString());
+
+
+const myCreatedDate=new Date(2023,0,23);       // months starts from 0 ok
+console.log(myCreatedDate.toDateString());
+
+
