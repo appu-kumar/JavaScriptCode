63818e0b8300228f70f64e26aa6bf3ef658c0296
appu-kumar <83134946+appu-kumar@users.noreply.github.com>
2024-02-06 15:17:20 +0530

    functions theory basics

---
+++ function1.js
@@ -0,0 +1,28 @@
+
+
+
+function adding(){
+      console.log("adding");
+}
+
+function add1(num1,num2){                             // parameters-> num1,num2
+    console.log(num1+num2);
+}
+
+function add2(num1,num2){                           
+    return num1+num2;
+}
+
+function add3(...num)                 // rest or spread operator accn to uses we give the name 
+{
+    return num;
+}
+
+adding();
+add1(2,3);  
+console.log(add3(3,3,3,3,3,3,3,3,3,3));  
+                        
+
+
+const ans=add2;          // ans will store the reference of the function.
+console.log(add2(2,3));  // arguments
