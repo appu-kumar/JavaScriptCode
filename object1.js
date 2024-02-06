c97760f8d7bca7dc36181f7060979f309834e9ad
appu-kumar <83134946+appu-kumar@users.noreply.github.com>
2024-02-06 11:21:19 +0530

    Object in js part 1

---
+++ object1.js
@@ -0,0 +1,42 @@
+
+// create object using class baad me dekhenge
+
+
+
+// create object using literals
+
+const symbolKey=Symbol("myphoto");
+
+const jsUser={
+
+    id:1,                              // id ---->"id" after compilation
+    name:"appu kumar",
+    subjects:["maths","science","English"],
+    emailId:"appu6688kumar@gmail.com",
+    isLoggedIn:true,
+    fees:2353.5,
+    "core subject":"maths",
+    [symbolKey]:"photoId",
+
+}
+
+console.log(jsUser);
+
+// how to access values 1).  2)[]
+console.log(jsUser.id);               // it might be unsafe, suppose key is declared like this "core subjects" then . operator will not work.
+console.log(jsUser["id"]);           // safe way 
+
+  // console.log(jsUser."core subject");        // this gives error okay
+  console.log(jsUser["core subject"]);          // safe way to access value okay
+  console.log(jsUser[symbolKey]);    
+
+  jsUser.greetings=function (){                // adding function in the jsUser object
+         console.log("hello appu kumar");
+  }
+
+  ///console.log(jsUser.greetings);            // this prints reference of the function
+  console.log(jsUser.greetings());
+
+
+
+
